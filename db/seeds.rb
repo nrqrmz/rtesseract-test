0c20ec578c6a337d878cc6524b91351d66b16c74
Enrique Ramirez <nrqrmz@gmail.com>
2024-06-26 08:56:56 -0500

    add 4 test tickets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 # install Tesseract OCR
-# sudo apt-get install tesseract-ocr # ubunto
+# sudo apt-get install tesseract-ocr # ubuntu
 # brew install tesseract # macOS
 
-require 'rtesseract'
+# require 'rtesseract'
 
-ticket_1_path = File.join(Dir.pwd, 'app', 'assets', 'images', 'ticket_1.jpg')
-ticket_1 = RTesseract.new(ticket_1_path)
+tickets = %w[ticket_1.jpg ticket_2.jpg ticket_3.jpg ticket_3.jpg ticket_4.jpg]
 
-ticket_1_text = ticket_1.to_s # This converts the recognized text to a string
+tickets.each do |ticket|
+  ticket_path = File.join(Dir.pwd, 'app', 'assets', 'images', ticket)
+  ocr_ticket = RTesseract.new(ticket_path)
 
-puts ticket_1_text # Prints the extracted text to your console
+  recognized_text = ocr_ticket.to_s
+
+  puts recognized_text
+
+  puts
+  puts '-' * 50
+  puts
+end
